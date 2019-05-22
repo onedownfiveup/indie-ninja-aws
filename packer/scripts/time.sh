5ac93784c26232a59eb8e2944e482bc8e18ff494
Constantinos (Gus) Mavromoustakos <cmavromoustakos@gmail.com>
2019-05-22 11:55:41 -0400

    Use chrony for time sync

--- packer/scripts/time.sh
+++ packer/scripts/time.sh
@@ -3,13 +3,23 @@ set -e
 
 # Configure host to use timezone
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/set-time.html
+
 echo "### Setting timezone to $TIMEZONE ###"
+sudo yum install -y chrony
 sudo tee /etc/sysconfig/clock << EOF > /dev/null
 ZONE="$TIMEZONE"
 UTC=true
 EOF
 
+sudo tee /etc/chrony.conf << EOF > /dev/null
+server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4
+EOF
+
+echo "### Linking timezone file /usr/share/zoneinfo/$TIMEZONE ###"
 sudo ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
 
-# Enable NTP
-sudo chkconfig ntpd on
+sudo systemctl enable chronyd.service
+sudo systemctl start chronyd.service
+
+echo "### Checking chrony config ###"
+sudo chronyc tracking
